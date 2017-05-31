5bbeb828b6a699f3211fd863b0e2931075d88701
DESKTOP-955G2G9\ccc <qma@cccis.com>
2017-05-31 11:43:59 +0800

    add bower file.

--- src/main/java/com/cccis/oa/wechat/action/PartAction.java
+++ src/main/java/com/cccis/oa/wechat/action/PartAction.java
@@ -8,9 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.cccis.base.decrypt.CDS;
 import com.cccis.oa.wechat.model.PartModel;
-import com.cccis.oa.wechat.service.GZipReader;
 
 /**
  * Created by CCC on 2016/10/25.
@@ -23,7 +21,6 @@ public class PartAction {
     private static String key = "29";
 
     public PartAction() {
-        partModels = GZipReader.getParts();
     }
 
     @RequestMapping(value = "/apply", method = RequestMethod.GET)
@@ -34,19 +31,6 @@ public class PartAction {
             extNum = number;
         }
         long s = System.currentTimeMillis();
-        for(int i=0; i < extNum; i++) {
-            PartModel partModel = partModels.get(i);
-            //System.out.println(partModel);
-            String laborType = CDS.decrypt(partModel.getLaborType(), key);
-            String texture = CDS.decrypt(partModel.getTexture(), key);
-            String repairPartFlag = CDS.decrypt(partModel.getRepairPartFlag(), key);
-            String paintFlag = CDS.decrypt(partModel.getPaintFlag(), key);
-            String lossDegree = CDS.decrypt(partModel.getLossDegree(), key);
-            String easyAbradedFlag = CDS.decrypt(partModel.getEasyAbradedFlag(), key);
-            String assemblyPartFlag = CDS.decrypt(partModel.getAssemblyPartFlag(), key);
-            /*System.out.println("PartName:"+ partModel.getPartName() + ", laborType:" +laborType + " , texture:" + texture +" , repairPartFlag:" + repairPartFlag + " , paintFlag:"
-                    + paintFlag + " , lossDegree:"+lossDegree + " , easyAbradedFlag:" + easyAbradedFlag+ " , assemblyPartFlag:" + assemblyPartFlag);*/
-        }
         long t = System.currentTimeMillis() - s;
         return "Decrypt total " + extNum + ", take time " + t + "(ms)";
     }
